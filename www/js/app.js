e1126c1be272bd3e84cc4eb0d1244196bbc2458e
Hendy Irawan <ceefour666@gmail.com>
2014-03-27 10:41:54 -0430

    final

---
+++ www/js/app.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $('#weatherBtn').click(function() {
+        var city = $('#cityFld').val();
+        var countryCode = $('#countryCodeFld').val();
+        $.getJSON('http://api.openweathermap.org/data/2.5/weather',
+            {
+                q: city + ',' + countryCode,
+                units: 'metric'
+            }, function(data) {
+                console.debug('OpenWeatherMap data for ', city, countryCode, '>', data);
+                $('#weatherFld').val( data.weather[0].description );
+                $('#tempFld').val( data.main.temp_min + ' - ' + data.main.temp_max + ' °C');
+            });
+            
+    });
+});
